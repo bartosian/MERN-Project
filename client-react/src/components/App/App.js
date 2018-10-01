9f92abbe12e64fefb9bed92907b30dcb2eaaed8a
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-01 16:42:36 -0400

    Finished logeddin

--- client-react/src/components/App/App.js
+++ client-react/src/components/App/App.js
@@ -2,17 +2,40 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Auth from '../Auth/Auth';
 import Profile from '../Profile/Profile';
+import AuthService from '../../services/auth-service';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
-    return (
-        <Switch>
-            <Route path='/profile' component={ Profile }/>
-            <Route path='/' component={ Auth } />
-        </Switch>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = { loggedInUser: null };
+        this.service = new AuthService();
+    }
+
+    fetchUser = () => {
+        if( this.state.loggedInUser === null ){
+            this.service.loggedin()
+                .then(response =>{
+                    this.setState({
+                        loggedInUser:  response
+                    })
+                })
+                .catch( err =>{
+                    this.setState({
+                        loggedInUser:  false
+                    })
+                })
+        }
+    };
+
+      render() {
+        return (
+            <Switch>
+                <Route path='/profile' component={ Profile }/>
+                <Route path='/' component={ Auth } />
+            </Switch>
+        );
+      }
 }
 
 export default App;
