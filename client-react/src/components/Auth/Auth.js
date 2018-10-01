f30eef708cfe01cb24b20ed25d4bc5448b851988
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-01 15:57:08 -0400

    Finished auth structure

--- client-react/src/components/Auth/Auth.js
+++ client-react/src/components/Auth/Auth.js
@@ -5,14 +5,19 @@ import Login from './Login/Login';
 import './Auth.css';
 
 const auth = (props) => (
-    <div className="col-12 col-md-5 user-form">
-        <Switch>
-            <Route exact path="/" render={ () => (
-                <Redirect to="/signup"/>
-            )} />
-            <Route path="/signup" component={ Signup }/>
-            <Route path="/login" component={ Login }/>
-        </Switch>
+    <div className="container auth-wrapper">
+        <div className="row form-wrapper">
+            <div className="col-12 d-none d-md-block col-md-5"><img className="main-bg" src="http://factorybuilderstores.com/wp-content/uploads/2016/02/SM-Graphic.png" alt="social"/></div>
+            <div className="col-12 col-md-5 user-form">
+                <Switch>
+                    <Route exact path="/" render={ () => (
+                        <Redirect to="/signup"/>
+                    )} />
+                    <Route path="/signup" component={ Signup }/>
+                    <Route path="/login" component={ Login }/>
+                </Switch>
+            </div>
+        </div>
     </div>
 );
 
