544a61c401499b9bfb28e113f754cd8cdbc1955b
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-01 13:27:58 -0400

    Added auth component

---
+++ client-react/src/components/App/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+class App extends Component {
+  render() {
+    return (
+        <h1 className="text-center">Hi world</h1>
+    );
+  }
+}
+
+export default App;
