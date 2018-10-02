8e2492a6e6af62920fda28a32d8c455775065d8d
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-02 09:09:05 -0400

    Finishe pisture choose style

--- client-react/src/components/Profile/PictureBlock/PictureBlock.js
+++ client-react/src/components/Profile/PictureBlock/PictureBlock.js
@@ -7,12 +7,13 @@ class PictureBlock extends Component {
         return (
             <div className="picture-user">
                 <div className="choose-picture">
+                    <i className="fa fa-hand-o-down finger" aria-hidden="true"></i>
                     <p className="upload-hint">Choose photo</p>
                     <i className="fa fa-camera-retro camera"></i>
                     <input className="photo-loader" name="picture"  type="file"/>
                 </div>
-                button
 
+                button
             </div>
         );
     }
