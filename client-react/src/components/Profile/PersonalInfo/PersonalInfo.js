24471c987b0e57318144f347fa7cfd33f13ab2d7
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-02 16:42:30 -0400

    Finished info section

--- client-react/src/components/Profile/PersonalInfo/PersonalInfo.js
+++ client-react/src/components/Profile/PersonalInfo/PersonalInfo.js
@@ -18,12 +18,19 @@ class PersonalInfo extends Component {
                         <div className="row personal-details">
                             <div className="col-12 col-md-4 personal-detail">
                                 <h5><i className="fa fa-birthday-cake detail-icon text-primary"></i>Date of Birth</h5>
+                                <p>15.12.1989</p>
                             </div>
                             <div className="col-12 col-md-4 personal-detail">
                                 <h5><i className="fa fa-heart detail-icon text-primary"></i>Status</h5>
+                                <p>married</p>
                             </div>
                             <div className="col-12 col-md-4 personal-detail">
                                 <h5><i className="fa fa-address-book detail-icon text-primary"></i>Contact info</h5>
+                                <ul>
+                                    <li className='social-link'><i className="fa fa-envelope text-danger"></i>Gmail</li>
+                                    <li className='social-link'><i className="fa fa-linkedin text-secondary"></i>Linkedin</li>
+                                    <li className='social-link'><i className="fa fa-skype text-primary"></i>Scype</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
