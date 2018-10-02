e167aa926d022b47e972d276816e58a950af511e
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-02 17:10:01 -0400

    Finished interest section

--- client-react/src/components/Profile/PersonalInfo/PersonalInfo.js
+++ client-react/src/components/Profile/PersonalInfo/PersonalInfo.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 import './PersonalInfo.css';
 
 class PersonalInfo extends Component {
-
+    state = {
+        interests: [
+            'web-development',
+            'cars',
+            'music',
+            'friends',
+            'fights',
+            'fishing',
+            'drinks',
+            'money',
+            'technology',
+            'girls'
+        ]
+    };
 
 
     render() {
@@ -33,6 +46,15 @@ class PersonalInfo extends Component {
                                 </ul>
                             </div>
                         </div>
+                    </div>
+                </div>
+                <div className="col-12 user-interests">
+                    <div className="row interests">
+                        {
+                            this.state.interests.map((i, id) => (
+                                <div key={i + id} className="interest">{i}</div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
