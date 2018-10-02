907c043f97d12ee3795cd8ce258d4cb21a8d2edb
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-02 08:16:49 -0400

    Created layout profile

--- client-react/src/components/Profile/Profile.js
+++ client-react/src/components/Profile/Profile.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const profile = (props) => (
-    <h3>PROFILE PAGE</h3>
-);
+class Profile extends Component {
 
-export default profile;
+
+    render() {
+        return (
+            <div className="container p-5">
+                <div className="row">
+                    <div className="col-12 col-md-4">Picture options</div>
+                    <div className="col-12 col-md-8">Personal info</div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-4">Friends options</div>
+                    <div className="col-12 col-md-8">Board options</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Profile;
