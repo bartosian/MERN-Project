6b97610eb2a498b421919669a2ed42eef3a24559
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 16:02:39 -0400

    Implemented posts route

--- routes/posts.js
+++ routes/posts.js
@@ -4,8 +4,6 @@ const middleAuth = require('../middleWare/auth');
 const { Post } = require('../models/Post');
 const { User } = require('../models/User');
 
-
-
 /* Add new post */
 router.post('/posts', middleAuth, async function(req, res, next) {
     const { _id } = req.user;
@@ -27,5 +25,23 @@ router.post('/posts', middleAuth, async function(req, res, next) {
         return next(ex);
     }
 });
+
+/* Get friends posts */
+router.get('/friends/posts', middleAuth, async function(req, res, next) {
+    const { _id } = req.user;
+
+    try {
+        const user = await User.findById(_id).populate('friends');
+        const posts = user.friends.reduce((arr,friend) => {
+            return arr.push(...friend.posts);
+        }, []);
+
+        res.status(200)
+            .json(posts);
+    } catch(ex) {
+        return next(ex);
+    }
+});
+
 
 module.exports = router;
