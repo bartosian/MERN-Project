68e99addaec0bb03fa5424a6fcc08dffcfd804ad
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 08:36:24 -0400

    Added friend component

---
+++ client-react/src/components/Profile/Friends/Friend/Friend.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const friend = (props) => {
+
+
+    return (
+        <p>friend</p>
+    );
+};
+
+export default friend;
--- client-react/src/components/Profile/Friends/Friends.js
+++ client-react/src/components/Profile/Friends/Friends.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './Friends.css';
+import Friend from './Friend/Friend';
 
 const friends = (props) => {
 
+
+    return (
+        <p>Hello friends</p>
+    );
 };
 
 export default friends;
