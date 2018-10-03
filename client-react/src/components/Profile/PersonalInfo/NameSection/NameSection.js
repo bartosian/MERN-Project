0c44fafd72bd7244b753b2fc20461a0c1225682b
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-02 20:05:51 -0400

    Craeted details component

--- client-react/src/components/Profile/PersonalInfo/NameSection/NameSection.js
+++ client-react/src/components/Profile/PersonalInfo/NameSection/NameSection.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const name = (props) => {
 
+
+    return (
+        <Fragment>
+            <i className="fa fa-user-circle person-icon text-primary"></i>
+            <h1>User name</h1>
+        </Fragment>
+    )
 };
 
 export default name;
--- client-react/src/components/Profile/PersonalInfo/PersonalInfo.js
+++ client-react/src/components/Profile/PersonalInfo/PersonalInfo.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Name from '../../../components/Profile/PersonalInfo/NameSection/NameSection';
+import Details from '../../../components/Profile/PersonalInfo/PersonalDetails/PersonalDetails';
 import './PersonalInfo.css';
 
 class PersonalInfo extends Component {
@@ -23,28 +25,12 @@ class PersonalInfo extends Component {
         return (
             <div className="container">
                 <div className="col-12 user-name">
-                    <i className="fa fa-user-circle person-icon text-primary"></i>
-                    <h1>User name</h1>
+                    <Name />
                 </div>
                 <div className="user-info">
                     <div className="row summary">
                         <div className="row personal-details">
-                            <div className="col-12 col-md-4 personal-detail">
-                                <h5><i className="fa fa-birthday-cake detail-icon text-primary"></i>Date of Birth</h5>
-                                <p>15.12.1989</p>
-                            </div>
-                            <div className="col-12 col-md-4 personal-detail">
-                                <h5><i className="fa fa-heart detail-icon text-primary"></i>Status</h5>
-                                <p>married</p>
-                            </div>
-                            <div className="col-12 col-md-4 personal-detail">
-                                <h5><i className="fa fa-address-book detail-icon text-primary"></i>Contact info</h5>
-                                <ul>
-                                    <li className='social-link'><i className="fa fa-envelope text-danger"></i>Gmail</li>
-                                    <li className='social-link'><i className="fa fa-linkedin text-secondary"></i>Linkedin</li>
-                                    <li className='social-link'><i className="fa fa-skype text-primary"></i>Scype</li>
-                                </ul>
-                            </div>
+                            <Details />
                         </div>
                     </div>
                 </div>
