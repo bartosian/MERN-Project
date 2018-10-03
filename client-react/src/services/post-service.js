979ce60de9a0acf605bd3d1b6fc54f541383447a
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 17:04:14 -0400

    Created posts service

---
+++ client-react/src/services/post-service.js
@@ -0,0 +1,35 @@
+
+import axios from 'axios';
+
+class PostService {
+    constructor() {
+        let postService = axios.create({
+            baseURL: 'http://localhost:5000/api',
+            withCredentials: true
+        });
+        this.postService = postService;
+    }
+
+    addPost = ({content}) => {
+        return this.postService.post('/posts', {content})
+            .then(response => response.data)
+    };
+
+    editPost = ({content, id}) => {
+        return this.postService.put(`/posts/${id}`, {content})
+            .then(response => response.data)
+    };
+
+    deletePost = ({id}) => {
+        return this.postService.delete(`/posts/${id}`)
+            .then(response => response.data)
+    };
+
+    getFriendsPosts = () => {
+        return this.postService.get('/friends/posts')
+            .then(response => response.data)
+    };
+
+}
+
+export default PostService;
