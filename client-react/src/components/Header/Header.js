4e89ecb23ada2ce8aa2de2dca7a2960860a13ab4
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 13:59:29 -0400

    Finished layout header

--- client-react/src/components/Header/Header.js
+++ client-react/src/components/Header/Header.js
@@ -1,33 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 import Input from '../UI/Input/Input';
 import Button from '../UI/Button/Button';
 
-const header = (props) => {
+class Header extends Component {
 
-    return (
-        <div className="container">
-            <div className="col-12 header-comp">
-                <div className="logo-bar">
-                    <div className="logo">
-                        <img className="logo-img" src="http://www.broutinwebpublishing.com/wp-content/uploads/2016/04/social-media-local-1024x723.png" alt="logo"/>
+    state = {
+      search: ""
+    };
+
+    inputChangedHandler = (e) => {
+        const { name, value } = e.target;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+
+    render() {
+        return (
+            <div className="container">
+                <div className="col-12 header-comp">
+                    <div className="logo-bar">
+                        <div className="logo">
+                            <img className="logo-img" src="http://www.broutinwebpublishing.com/wp-content/uploads/2016/04/social-media-local-1024x723.png" alt="logo"/>
+                        </div>
+                        <h1 className="main-logo--navbar">InTouch</h1>
                     </div>
-                    <h1 className="main-logo--navbar">InTouch</h1>
+                    <nav className="main-nav">
+                        <div className="search">
+                            <Input
+                                elementType="input"
+                                elementConfig={{ placeholder: "Search", name: "search", type: "text" }}
+                                value={ this.state.search }
+                                changed={ (event) => this.inputChangedHandler(event) }
+                            />
+                        </div>
+                        <div className="logout">
+                            <Button btnType="danger">Logout</Button>
+                        </div>
+                    </nav>
+
                 </div>
-                <nav className="main-nav">
-                    <div className="search">
-                        <Input />
-                        <Button />
-                    </div>
-                    <div className="logout">
-                        <Button />
-                    </div>
-                </nav>
-
             </div>
-        </div>
-    );
-
+        );
+    }
 };
 
-export default header;
+export default Header;
