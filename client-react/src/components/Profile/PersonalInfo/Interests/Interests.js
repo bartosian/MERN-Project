994071e62b2cfd47dad714e5c4b88d7601147a19
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-02 20:57:32 -0400

    Added boilerplates

--- client-react/src/components/Profile/PersonalInfo/Interests/Interests.js
+++ client-react/src/components/Profile/PersonalInfo/Interests/Interests.js
@@ -1,14 +1,21 @@
 import React, { Fragment } from 'react';
+import './Interests.css';
 
 const interests = ({ interests }) => {
 
+    const interestList = interests && (
+        <div className="int-boiler">
+            <i className="fa fa-list-alt" aria-hidden="true"></i>
+            <p>You can add your interests here</p>
+        </div>
+    );
 
     return (
         <Fragment>
             {
-                interests.map((i, id) => (
+                interestList.length ? interestList.map((i, id) => (
                     <div key={i + id} className="interest">{i}</div>
-                ))
+                )) : interestList
             }
         </Fragment>
     )
--- client-react/src/components/Profile/PersonalInfo/PersonalDetails/PersonalDetails.js
+++ client-react/src/components/Profile/PersonalInfo/PersonalDetails/PersonalDetails.js
@@ -1,25 +1,33 @@
 import React, { Fragment } from 'react';
+import  './PersonalDetails.css';
 
 const details = (props) => {
 
+    const DOB = props.dob || (
+        <i className="fa fa-search-plus empty-data" aria-hidden="true"></i>
+    );
+
+    const status = props.status || (
+        <i className="fa fa-search-plus empty-data" aria-hidden="true"></i>
+    );
+
+    const contacts = (props.contacts && props.contacts.length) || (
+        <i className="fa fa-search-plus empty-data" aria-hidden="true"></i>
+    );
 
     return (
         <Fragment>
             <div className="col-12 col-md-4 personal-detail">
                 <h5><i className="fa fa-birthday-cake detail-icon text-primary"></i>Date of Birth</h5>
-                <p>15.12.1989</p>
+                <p>{ DOB }</p>
             </div>
             <div className="col-12 col-md-4 personal-detail">
                 <h5><i className="fa fa-heart detail-icon text-primary"></i>Status</h5>
-                <p>married</p>
+                <p>{ status }</p>
             </div>
             <div className="col-12 col-md-4 personal-detail">
                 <h5><i className="fa fa-address-book detail-icon text-primary"></i>Contact info</h5>
-                <ul>
-                    <li className='social-link'><i className="fa fa-envelope text-danger"></i>Gmail</li>
-                    <li className='social-link'><i className="fa fa-linkedin text-secondary"></i>Linkedin</li>
-                    <li className='social-link'><i className="fa fa-skype text-primary"></i>Scype</li>
-                </ul>
+                <p>{ contacts }</p>
             </div>
         </Fragment>
     )
