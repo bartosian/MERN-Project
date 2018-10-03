1f272bbae86cdf7a5ec90725d333e6a9720522dd
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 10:39:03 -0400

    Added addPost component

--- client-react/src/components/Profile/Wall/Wall.js
+++ client-react/src/components/Profile/Wall/Wall.js
@@ -3,11 +3,18 @@ import './Wall.css';
 
 class Wall extends Component {
 
+    state = {
+        posts: []
+    };
+
 
     render() {
         return (
             <div className="wall-wrapper">
                 <h5 className="friends-header">Posts</h5>
+                <div className="wall-posts">
+
+                </div>
             </div>
         );
     }
