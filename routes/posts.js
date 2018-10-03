5642a04c4dbe4c7f28b11514aea52323c2a93567
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 15:54:14 -0400

    Add new post works

--- routes/posts.js
+++ routes/posts.js
@@ -2,48 +2,27 @@ const express = require('express');
 const router  = express.Router();
 const middleAuth = require('../middleWare/auth');
 const { Post } = require('../models/Post');
+const { User } = require('../models/User');
 
 
-/* Get events info */
-router.get('/events', authToken, async function(req, res, next) {
-    const { _id } = req.user;
-
-    try {
-        const user = await User.findById(_id);
-        let events = user.events;
-
-
-        return res.status(200)
-            .json(
-                events
-            );
-    } catch(ex) {
-        return next(ex);
-    }
-});
 
 /* Add new post */
-router.post('/events', authToken, async function(req, res, next) {
+router.post('/posts', middleAuth, async function(req, res, next) {
     const { _id } = req.user;
-    let { type, amount, category, date, description } = req.body;
+    let { content } = req.body;
 
     try {
         const user = await User.findById(_id);
-        const newEvent = new Event({
-            type,
-            amount,
-            category,
-            date,
-            description
+        const newPost = new Post({
+            user: _id,
+            content
         });
 
-        user.events.push(newEvent);
+        user.posts.push(newPost);
         await user.save();
 
-        return res.status(201)
-            .json({
-                "message": "Event was added successfully!"
-            });
+         res.status(201)
+            .json(newPost);
     } catch(ex) {
         return next(ex);
     }
