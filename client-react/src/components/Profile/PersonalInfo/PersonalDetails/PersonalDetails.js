92004a8554c91729e4c3a1761590aaf90ccf13b6
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-03 20:45:34 -0400

    Fixed personal details

--- client-react/src/components/Profile/PersonalInfo/PersonalDetails/PersonalDetails.js
+++ client-react/src/components/Profile/PersonalInfo/PersonalDetails/PersonalDetails.js
@@ -1,17 +1,17 @@
 import React, { Fragment } from 'react';
 import  './PersonalDetails.css';
 
-const details = (props) => {
+const details = ({dob, status, contacts}) => {
 
-    const DOB = props.dob || (
+    const DOB = "20.12.1989" || (
         <i className="fa fa-search-plus empty-data" aria-hidden="true"></i>
     );
 
-    const status = props.status || (
+    const Status = status || (
         <i className="fa fa-search-plus empty-data" aria-hidden="true"></i>
     );
 
-    const contacts = (props.contacts && props.contacts.length) ? props.contacts : (
+    const Contacts = (contacts && contacts.length) ? contacts : (
         <i className="fa fa-search-plus empty-data" aria-hidden="true"></i>
     );
 
@@ -23,11 +23,11 @@ const details = (props) => {
             </div>
             <div className="col-12 col-md-4 personal-detail">
                 <h5><i className="fa fa-heart detail-icon text-primary"></i>Status</h5>
-                <p>{ status }</p>
+                <p>{ Status }</p>
             </div>
             <div className="col-12 col-md-4 personal-detail">
                 <h5><i className="fa fa-address-book detail-icon text-primary"></i>Contact info</h5>
-                <p>{ contacts }</p>
+                <p>{ Contacts }</p>
             </div>
         </Fragment>
     )
