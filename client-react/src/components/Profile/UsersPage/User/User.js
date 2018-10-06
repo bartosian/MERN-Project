fb049f2d78e02576c12132f204ff8d6b3796a924
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-06 10:00:30 -0400

    Done info friend

--- client-react/src/components/Profile/UsersPage/User/User.js
+++ client-react/src/components/Profile/UsersPage/User/User.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './User.css';
 
 const user = ({user}) => {
-    const {image} = user;
+    const {image, username, status, country, occupation } = user;
     return (
         <div className="row friend-block">
             <div className="col-12 col-md-7 friend-main">
@@ -12,7 +12,16 @@ const user = ({user}) => {
                     </div>
                     <img src={image || "https://yt3.ggpht.com/a-/AJLlDp02y_3SsMYN_uiJd9sGjNL0fFeCjsQhSW90=s900-mo-c-c0xffffffff-rj-k-no"} alt="friend"/>
                 </div>
-                <div className="friend-info"></div>
+                <div className="friend-info">
+                    <div className="friend-name-block">
+                        <p className="friend-name"><i className="fa fa-user-circle detail-icon text-primary"></i>{ username || "Not specified" }</p>
+                        <p><i className="fa fa-globe detail-icon text-primary"></i>{ country || "Not specified" }</p>
+                    </div>
+                    <div className="friend-name-block">
+                        <p className="friend-status"><i className="fa fa-heart detail-icon text-primary"></i>{ status || "Not specified" }</p>
+                        <p><i className="fa fa-briefcase detail-icon text-primary"></i>{ occupation || "Not specified" }</p>
+                    </div>
+                </div>
                 <div className="friend-controls"></div>
             </div>
         </div>
