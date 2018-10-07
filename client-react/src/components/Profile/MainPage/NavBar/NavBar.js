fd950edcdeff79dc9a552de859560c2630064308
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-07 09:51:50 -0400

    Done navbar

--- client-react/src/components/Profile/MainPage/NavBar/NavBar.js
+++ client-react/src/components/Profile/MainPage/NavBar/NavBar.js
@@ -6,7 +6,12 @@ const navBar = (props) => {
 
     return (
         <div className="navBar-main">
-
+            <div className="navBar-links">
+                <NavLink  className="main-link" activeClassName="active-link" to="/profile/edit">Settings</NavLink>
+                <NavLink  className="main-link" activeClassName="active-link" to="/profile/users">Users</NavLink>
+                <NavLink  className="main-link" activeClassName="active-link" to="/profile/news">News</NavLink>
+                <NavLink  className="main-link" activeClassName="active-link" to="/profile/messages">Messages</NavLink>
+            </div>
         </div>
     );
 };
