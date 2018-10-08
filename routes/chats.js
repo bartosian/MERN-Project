5b58f2a3c9fe448430488425bee57bd893ad2a11
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-08 09:28:11 -0400

    Created delete chat

--- routes/chats.js
+++ routes/chats.js
@@ -58,5 +58,54 @@ router.post('/chats', middleAuth, async function(req, res, next) {
 
 });
 
+/* Delete  chat */
+router.delete('/chats/:id', middleAuth, async function(req, res, next) {
+    const { _id } = req.user;
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        res.status(400)
+            .json({ message: 'Specified chat id is not valid' });
+        return;
+    }
+
+    try {
+
+        const chat = await Chat.findById(id).select("speakerFirst speakerSecond");
+        const userOne = await User.findById(chat.speakerFirst).select("chats");
+        const userTwo = await User.findById(chat.speakerSecond).select("chats");
+
+        userOne.chats = userOne.chats.filter(chat => {
+            return String(chat) !== String(chat._id);
+        });
+
+        await userOne.save();
+
+        userTwo.chats = userTwo.chats.filter(chat => {
+            return String(chat) !== String(chat._id);
+        });
+
+        await userTwo.save();
+
+        await Chat.findByIdAndRemove(chat._id);
+
+        const resultUser = await User.findById(_id).select("chats").populate({
+            path: 'chats',
+            model: 'Chat',
+            populate: {
+                path: 'speakerFirst speakerSecond',
+                model: 'User'
+            }
+        });
+
+        res.status(201)
+            .json(resultUser.chats);
+
+    } catch(ex) {
+        return next(ex);
+    }
+
+});
+
 
 module.exports = router;
