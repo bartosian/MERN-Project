0ad101d75f4045ffe078076321aff71e1e0836cc
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-09 17:00:23 -0400

    Fixed routes

--- client-react/src/components/Profile/NewsPage/SingleNews/SingleNews.js
+++ client-react/src/components/Profile/NewsPage/SingleNews/SingleNews.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Singlenews.css';
 import moment from 'moment';
 import { withRouter } from 'react-router';
+import noUser from '../../../../assets/images/no-user.jpg';
 
 const singleNews = ({image, username, content, date, likes, user, _id, addLike, selectArticle, history }) => {
     let newDate = new Date(date);
@@ -16,7 +17,7 @@ const singleNews = ({image, username, content, date, likes, user, _id, addLike, 
         <div className="row news-wrapper">
             <div className="col-10 col-md-8 news-main">
                 <div className="news-image">
-                    <img src={ image ? image : "https://yt3.ggpht.com/a-/AJLlDp02y_3SsMYN_uiJd9sGjNL0fFeCjsQhSW90=s900-mo-c-c0xffffffff-rj-k-no" } alt=""/>
+                    <img src={ image ? image : noUser } alt=""/>
                 </div>
                 <div className="news-info">
                     <div className="news-user">
