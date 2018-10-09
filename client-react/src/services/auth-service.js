cf271b3b5ae1ce5da1c6f9e02df0e3fc1404fd7f
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-09 13:26:16 -0400

    Third commit on heroku

--- client-react/src/services/auth-service.js
+++ client-react/src/services/auth-service.js
@@ -4,29 +4,29 @@ import axios from 'axios';
 class AuthService {
     constructor() {
         let service = axios.create({
-            baseURL:`https://be-in-touch.herokuapp.com/Auth`,
+            baseURL:`https://be-in-touch.herokuapp.com`,
             withCredentials: true
         });
         this.service = service;
     }
 
     signup = ({username, password, email} ) => {
-        return this.service.post('/signup', {username, password, email })
+        return this.service.post('/Auth/signup', {username, password, email })
             .then(response => response.data)
     };
 
     loggedin = () => {
-        return this.service.get('/loggedin')
+        return this.service.get('/Auth/loggedin')
             .then(response => response.data)
     };
 
     login = ({email, password}) => {
-        return this.service.post('/login', {email, password})
+        return this.service.post('/Auth/login', {email, password})
             .then(response => response.data)
     };
 
     logout = () => {
-        return this.service.post('/logout', {})
+        return this.service.post('/Auth/logout', {})
             .then(response => response.data)
     };
 
@@ -35,7 +35,7 @@ class AuthService {
         formData.append("picture", file);
         console.log('DEBUG formData', formData.get("picture"));
         return this.service
-            .post('/upload', formData, {
+            .post('/Auth/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
