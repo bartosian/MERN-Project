5c1b335ca727ed289ab1e7e96340fcf20d213d1a
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-10 08:51:49 -0400

    Fixed styles icons

--- client-react/src/components/Profile/UsersPage/User/User.js
+++ client-react/src/components/Profile/UsersPage/User/User.js
@@ -49,12 +49,12 @@ const user = ({user, userFriends, selectPhoto, addFriend, deleteFriend, history}
                 <div className="friend-info">
                     <div className="friend-name-block">
                         <p className="friend-name"><i className="fa fa-user-circle detail-icon text-primary"></i>{ username || "Not specified" }</p>
-                        <p><i className="fa fa-globe detail-icon text-primary"></i>{ country || "Not specified" }</p>
+                        <p className="friend-country"><i className="fa fa-globe detail-icon text-primary"></i>{ country || "Not specified" }</p>
                         <p className="friends-quantity-user"><i className="fa fa-users detail-icon text-primary"></i>{ friends.length }</p>
                     </div>
                     <div className="friend-name-block">
                         <p className="friend-status"><i className="fa fa-heart detail-icon text-primary"></i>{ status || "Not specified" }</p>
-                        <p><i className="fa fa-briefcase detail-icon text-primary"></i>{ occupation || "Not specified" }</p>
+                        <p className="friend-occupation"><i className="fa fa-briefcase detail-icon text-primary"></i>{ occupation || "Not specified" }</p>
                     </div>
                 </div>
                 { controls }
